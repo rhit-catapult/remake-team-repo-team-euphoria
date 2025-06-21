cc9217bb72b3a6108b7e81b40029cddbf92e772c
glassSnowBa11 <berreszachary@gmail.com>
2025-06-21 10:48:57 -0400

    added pngs

--- my_character.py
+++ my_character.py
@@ -2,36 +2,37 @@ import pygame
 import sys
 
 
-class Character:
+class Kyle:
     def __init__(self, screen: pygame.Surface, x, y):
         self.screen = screen
         self.x = x
         self.y = y
+        self.image = pygame.image.load(kyle)
+        self.last_hit_time = 0
 
-    def draw(self):
-        pygame.draw.rect(self.screen, "blue", (self.x, self.y, 20, 20))
-        pygame.draw.circle(self.screen, "red", (self.x + 5, self.y + 5), 3)
-        pygame.draw.circle(self.screen, "red", (self.x + 15, self.y + 5), 3)
+    #def draw(self):
 
 
-# This function is called when you run this file, and is used to test the Character class individually.
-# When you create more files with different classes, copy the code below, then
-# change it to properly test that class
+
+
 def test_character():
     # TODO: change this function to test your class
     screen = pygame.display.set_mode((640, 480))
-    character = Character(screen, 400, 400)
+    character = Kyle(screen, 400, 400)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         screen.fill("white")
-        character.draw()
+
         pygame.display.update()
 
 
-# Testing the classes
-# click the green arrow to the left or run "Current File" in PyCharm to test this class
+
+
+
+
+
 if __name__ == "__main__":
     test_character()
