e641666fabb3d1c2d815127ee05aeb09f85c2aa2
glassSnowBa11 <berreszachary@gmail.com>
2025-06-21 16:01:26 -0400

    made kyle move

--- my_character.py
+++ my_character.py
@@ -3,14 +3,15 @@ import sys
 
 
 class Kyle:
-    def __init__(self, screen: pygame.Surface, x, y):
+    def __init__(self, screen: pygame.Surface, x, y, image_filename):
         self.screen = screen
         self.x = x
         self.y = y
-        self.image = pygame.image.load(kyle)
-        self.last_hit_time = 0
+        self.image = pygame.image.load(image_filename)
 
-    #def draw(self):
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
 
 
 
@@ -18,14 +19,24 @@ class Kyle:
 def test_character():
     # TODO: change this function to test your class
     screen = pygame.display.set_mode((640, 480))
-    character = Kyle(screen, 400, 400)
+    kyle = Kyle(screen, 300, 50, "kyle.png")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_LEFT]:
+            kyle.x = kyle.x - 0.4
+        if pressed_keys[pygame.K_RIGHT]:
+            kyle.x = kyle.x + 0.4
+        if pressed_keys[pygame.K_UP]:
+            kyle.y = kyle.y - 0.4
+        if pressed_keys[pygame.K_DOWN]:
+            kyle.y = kyle.y + 0.4
 
-        screen.fill("white")
+        screen.fill("gray")
 
+        kyle.draw()
         pygame.display.update()
 
 
